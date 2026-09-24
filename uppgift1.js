c6dae58b8e6b801872bfe7a69386a156a8ddb35c
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 16:15:39 +0200

    Skapade en kommentar till uppgiften och gjorde variablerna

---
+++ uppgift1.js
@@ -0,0 +1,9 @@
+/* Uppgift 1, lösning. Benjamin Björkström 2026 */
+"use strict"; 
+
+// Skapar variabler med information om en person//
+const firstName = "Benjamin";
+const lastName = "Björkström";
+const age = 20;
+const isStudent = true; 
+
