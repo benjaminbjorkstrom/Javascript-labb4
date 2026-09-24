1b52bbbc58f4f7e9b3eaceded67ab3f54798ae23
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 16:20:27 +0200

    skapade personinformationen i filen

--- uppgift1.js
+++ uppgift1.js
@@ -1,9 +1,13 @@
 /* Uppgift 1, lösning. Benjamin Björkström 2026 */
 "use strict"; 
 
-// Skapar variabler med information om en person//
+// Skapar variabler med information om en person. 
 const firstName = "Benjamin";
 const lastName = "Björkström";
 const age = 20;
 const isStudent = true; 
 
+// Skriver ut personinformation. 
+console.log(firstName + " " + lastName);
+console.log("Ålder: " + age);
+console.log("Student: " + isStudent);
