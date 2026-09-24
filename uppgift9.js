a5f6076dfd027cd903230abe10ed8576a737b14b
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 18:29:09 +0200

    Skapade en array med personer

--- uppgift9.js
+++ uppgift9.js
@@ -1,1 +1,21 @@
 /* Lösning till uppgift 9. Benjamin Björkström 2026 */
+"use strict";
+
+// Skapar en array med personer
+const people = [
+    {
+        name: "Martin"
+        age: 25,
+        city: "Stockholm"
+    },
+    {
+        name: "Elin"
+        age: 15,
+        city: "Göteborg"
+    },
+    {
+        name: "Alexander"
+        age: 40,
+        city: "Umeå"
+    }
+];
