faf9b0f1d46c696497329bab24d631856b4591f1
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 17:21:57 +0200

    Skapade första elementet i uppgift 5

--- uppgift5.js
+++ uppgift5.js
@@ -3,3 +3,9 @@
 
 //Skapar en array med maträtter.
 const meals = ["Sushi", "Tacos", "Pizza", "Lasagne", "Hamburgare"];
+
+// Skriver ut hela arrayen.
+console.log(meals);
+
+//Skriver ut det första elementet.
+console.log(meals[0]);
