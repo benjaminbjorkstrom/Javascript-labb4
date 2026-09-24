9a3ce312bd893d91e406503c9a1282931ff82c34
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 17:55:47 +0200

    Skapade en funktion som räknar ut summan av alla talen i arrayen

--- uppgift7.js
+++ uppgift7.js
@@ -3,3 +3,10 @@
 
 // Skapar en array med sex tal.
 const numbers = [5, 8, 3, 10, 7, 9];
+
+// Funktion som räknar ut summan av alla tal i arrayen.
+function calculateSum(numbers) {
+    let sum = 0;
+    
+
+}
