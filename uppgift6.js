950da3e14a3fe5447251266f32cc0e721686a974
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 17:42:23 +0200

    Skapade en funktion som räknar ut arean av en rektangel

---
+++ uppgift6.js
@@ -0,0 +1,7 @@
+/* Lösning till uppgift 5. Benjamin Björkström 2026 */
+"use strict"
+
+//Funktion som räknar ut arean av en rektangel.
+function calculateArea(width, height) {
+    return width * height;
+}
