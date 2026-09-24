bcd6cf004c84224ed3848561b365bc0c4fa347ad
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 18:10:13 +0200

    Skapade objekt som representerar en bok

--- uppgift8.js
+++ uppgift8.js
@@ -1,1 +1,9 @@
 /* Lösning till uppgift 8. Benjamin Björkström 2026 */
+"use strict";
+
+// Skapar ett objekt som representerar en bok.
+const book = {
+    title: "Harry potter och fången från Azkaban",
+    author: "J.K. Rowling",
+    publicationYear: "1999"
+}; 
