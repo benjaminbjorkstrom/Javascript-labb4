26954de63c632fb10a8179ac0302c9d275bc8c39
benjaminbjorkstrom <bebj2601@student.miun.se>
2026-09-24 17:12:02 +0200

    Lade till villkor för att kontrollera jämna tal i uppgift 4

--- uppgift4.js
+++ uppgift4.js
@@ -4,5 +4,10 @@
 // Loop som går igenom alla heltal från 1 till 20.
 for (let number = 1; number <= 20; number++) {
 
+    //Kontroll på om talet är jämnt.
+    if (number % 2 === 0) {
+        console.log(number);
+    }
+
 
 }
